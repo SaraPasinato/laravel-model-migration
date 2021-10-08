ff81a7032fac0e65300f171eab2b219ac65199ac
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-08 18:19:23 +0200

    fix typo

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'MyTravel | Contacts')
     
 @section('main-content')
-    <section id="contact" class="container-sm text-center">
+    <section id="contact" class="container-sm text-center mt-4">
         <h1>Ciao sno Contatti</h1>
     </section>
 @endsection
