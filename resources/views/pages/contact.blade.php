b1d7583738b360611238119fb3c5ca03c6b239b5
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-08 18:18:24 +0200

    add contact and active link

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'MyTravel | Contacts')
+    
+@section('main-content')
+    <section id="contact" class="container-sm text-center">
+        <h1>Ciao sno Contatti</h1>
+    </section>
+@endsection
