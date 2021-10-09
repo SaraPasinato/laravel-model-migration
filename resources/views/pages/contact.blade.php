9c6e1be77419cd03f8f5a5fabfb6ec2dcbf85814
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-09 14:48:30 +0200

    add contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.main')
 
 @section('title', 'MyTravel | Contacts')
-    
+
 @section('main-content')
-    <section id="contact" class="container-sm text-center mt-4">
-        <h1>Ciao sno Contatti</h1>
+    <section id="contact">
+        <div class="containers-sm text-center">
+            <div class="row h-100 d-flex align-items-center justify-content-between">
+                <div class="content w-50 mx-auto">
+                    <h1>Contact Us</h1>
+                        <p>Have more questions? We’ll find you the answers. Whether you’re ready to book a trip now or simply want to learn more about what Liberty Travel has to offer, our consultants are available to help.</p>
+                          <p class="title btn btn-primary my-4">New Bookings</p>
+                          <p class="subtitle">Ready to pack your bags for a vacation? Give us call to be matched with a travel consultant who will craft a trip just for you.</p>
+                </div>
+               <img class="w-50 h-100" src="https://www.libertytravel.com/sites/default/files/styles/full_size/public/contact%20us%20-%20bp3.jpg?itok=PDooNEhe" alt="Questions for Liberty Travel customer support">
+            </div>
+        </div>
     </section>
 @endsection
