d456c966c9e1f1dbe8095b8a7f05e99bbd81204f
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-09 14:49:36 +0200

    add contact page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
     <section id="home" >
         <div class="container-sm  d-flex  w-75 h-100  p-5  flex-column justify-content-center align-items-center text-light">
             <h1 class="text-uppercase ">Plan. Book. Relax.</h1>
-            <p class="lead">With our peace of mind flexible cancelletion plecies</p>
+            <p class="lead">With our peace of mind flexible cancellation pleacies</p>
             <p class="lead">
                 <a href="#" class="btn btn-lg btn-secondary fw-bold border-primary bg-primary">Learn more</a>
               </p>
